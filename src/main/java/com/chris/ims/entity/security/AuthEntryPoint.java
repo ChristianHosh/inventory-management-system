ee75301c2d8f7a37c6f397d25684488fd5cd7868
c.hosh <c.hosh@bisan.com>
2024-04-14 10:43:38 +0200

    - removed unused import

--- src/main/java/com/chris/ims/entity/security/AuthEntryPoint.java
+++ src/main/java/com/chris/ims/entity/security/AuthEntryPoint.java
@@ -2,7 +2,6 @@ package com.chris.ims.entity.security;
 
 import com.chris.ims.entity.exception.ApiError;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
